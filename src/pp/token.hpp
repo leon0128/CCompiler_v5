3c9b7043054fc29abcfb794f85f23f999985eeaa
leon0128 <18aj099@ms.dendai.ac.jp>
2020-02-09 12:55:50 +0900

    [update] Created a token class used by the preprocessor

---
+++ src/pp/token.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <string>
+
+class Token
+{
+public:
+    enum EClass
+    {
+        IDENTIFIER = 0,
+        PREPROCESSING_NUMBER = 1,
+        STRING_LITERAL = 2,
+        PUNCTUATOR = 3,
+        OTHER = 4
+    };
+
+    Token(const std::string& inData,
+          EClass inEClass):
+        data(inData),
+        eClass(inEClass){}
+
+    std::string data;
+    EClass eClass;
+};
